c4ab2c29fcd2073fbe536d75e84b5828d78099ab
Haidercc10 <cantillohaider10@gmail.com>
2023-07-25 10:02:24 -0500

    Cambios

--- Controllers/TransacController.cs
+++ Controllers/TransacController.cs
@@ -50,8 +50,8 @@ namespace ContabilidadZeusAPI.Controllers
         }
 
         // GET: api/Transac/5
-        [HttpGet("getCostosCuentasxMesDetallada/{anio}/{mes}")]
-        public ActionResult GetCostosCuentasxMesDetallada(string anio, string mes)
+        [HttpGet("getCostosCuentasxMesDetallada/{anio}/{mes}/{cuenta}")]
+        public ActionResult GetCostosCuentasxMesDetallada(string anio, string mes, string cuenta)
         {
             if (_context.Transacs == null) return NotFound();
 
@@ -63,12 +63,7 @@ namespace ContabilidadZeusAPI.Controllers
                           from ec in _context.Set<EscenariosCuenta>()
                           where tr.Idfuente == fu.Idfuente &&
                           tr.Codicta == ec.Id &&
-                          (tr.Codicta.StartsWith("51") ||
-                          tr.Codicta.StartsWith("52") ||
-                          tr.Codicta.StartsWith("53") ||
-                          tr.Codicta.StartsWith("72") ||
-                          tr.Codicta.StartsWith("73") ||
-                          tr.Codicta.StartsWith("74")) &&
+                          tr.Codicta == cuenta &&
                           tr.Statustra == "AC" &&
                           tr.Anotra == $"{anio}{mes}"
                           orderby tr.Codicta descending
